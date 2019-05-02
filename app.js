70338e830c77c15c33b401ec8b5d0b73fea82d8e
Jeremy Fourna <jeremyfourna@mykolab.com>
2019-05-02 16:41:21 +0200

    It works

--- app.js
+++ app.js
@@ -1,6 +1,50 @@
 const express = require('express');
+const fs = require('fs');
+const R = require('ramda');
 const app = express();
 const port = 3000;
+
+// log :: a -> a
+function log(content) {
+  console.log(...arguments);
+  return content;
+}
+
+function cleanNodeInput(args) {
+  return R.drop(2, args);
+}
+
+log(R.compose(
+  writeComposerData,
+  getComposerData,
+  R.head,
+  cleanNodeInput
+)(process.argv));
+
+function getComposerData(path) {
+  const composerJson = fs.readFileSync(R.concat(path, '/composer.json'), 'utf8');
+  const composerLock = fs.readFileSync(R.concat(path, '/composer.lock'), 'utf8');
+
+  return [{
+    path: './dist/my-composer-files/composerJSON.js',
+    data: composerJson,
+    stringStart: 'const myComposerJSON = ',
+    stringEnd: ';'
+  }, {
+    path: './dist/my-composer-files/composerLOCK.js',
+    data: composerLock,
+    stringStart: 'const myComposerLOCK = ',
+    stringEnd: ';'
+  }];
+}
+
+function writeComposerData(listOfFiles) {
+  R.forEach(cur => {
+    const p = R.prop(R.__, cur);
+    fs.writeFileSync(p('path'), `${p('stringStart')}${p('data')}${p('stringEnd')}`, 'utf8');
+  }, listOfFiles);
+}
+
 
 app.use(express.static('dist'));
 
