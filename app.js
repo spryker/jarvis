fb1bd929a91c94181cf26e4ad70f031b7667e1eb
Jeremy Fourna <jeremyfourna@mykolab.com>
2019-05-03 08:57:21 +0200

    Add compression and clean a bit

--- app.js
+++ app.js
@@ -1,53 +1,65 @@
 const express = require('express');
+const compression = require('compression');
 const fs = require('fs');
 const R = require('ramda');
 const app = express();
 const port = 3000;
 
+// This function does some IO
 // log :: a -> a
 function log(content) {
   console.log(...arguments);
   return content;
 }
 
+// This function does some IO
+// cleanNodeInput :: [string] -> [string]
 function cleanNodeInput(args) {
   return R.drop(2, args);
 }
 
-log(R.compose(
-  writeComposerData,
-  getComposerData,
-  R.head,
-  cleanNodeInput
-)(process.argv));
-
+// This function does some IO
+// getComposerData :: string -> [object]
 function getComposerData(path) {
-  const composerJson = fs.readFileSync(R.concat(path, '/composer.json'), 'utf8');
-  const composerLock = fs.readFileSync(R.concat(path, '/composer.lock'), 'utf8');
-
   return [{
     path: './dist/my-composer-files/composerJSON.js',
-    data: composerJson,
+    data: fs.readFileSync(R.concat(path, '/composer.json'), 'utf8'),
     stringStart: 'const myComposerJSON = ',
     stringEnd: ';'
   }, {
     path: './dist/my-composer-files/composerLOCK.js',
-    data: composerLock,
+    data: fs.readFileSync(R.concat(path, '/composer.lock'), 'utf8'),
     stringStart: 'const myComposerLOCK = ',
     stringEnd: ';'
   }];
 }
 
+// This function does some IO
+// writeComposerData :: [object] -> [object]
 function writeComposerData(listOfFiles) {
   R.forEach(cur => {
     const p = R.prop(R.__, cur);
     fs.writeFileSync(p('path'), `${p('stringStart')}${p('data')}${p('stringEnd')}`, 'utf8');
   }, listOfFiles);
+
+  return listOfFiles;
 }
 
+// Take the folder path and read/write the composer files
+R.compose(
+  writeComposerData,
+  getComposerData,
+  R.head,
+  cleanNodeInput
+)(process.argv);
 
+// Static files css/html/js
 app.use(express.static('dist'));
+// Compress for performance
+app.use(compression());
 
+// App has only 1 route
 app.get('/', (req, res) => res.sendFile('index.html'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Web server is listening
+app.listen(port, () => console.log(`Spryker Migration Analyzer is listening on http://localhost:${port}/`));
