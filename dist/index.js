3b2d0f242ecdb79ddd6187c2843e23a56ab28c27
Jeremy Fourna <jeremyfourna@mykolab.com>
2019-06-04 16:16:43 +0200

    Clean

--- dist/index.js
+++ dist/index.js
@@ -2,21 +2,8 @@
 // Functions to work with the Migration Analyzer //
 //////////////////////////////////////////////////
 
-
-// analyseMigrationToNextProductReleases :: (String, Object, Object, [Object]) -> HTML
-function analyseMigrationToNextProductReleases(el, currentComposer, currentComposerLock, currentFeatures) {
-    return render(el, migrateToNextProductReleases(currentComposer, currentComposerLock, currentFeatures));
-}
-
-// analyseMigrationToNextProductReleases :: (String, Object, Object, [Object]) -> HTML
-function analyseMigrationToNextModulesMajors(el, currentComposer, currentComposerLock, currentModules) {
-    return render(el, migrateModuleToNextMajor(currentComposer, currentComposerLock, currentModules));
-}
-
-function sprykerFeaturesYouDoNotUse(el, currentComposer, currentFeatures) {
-    return render(el, missingSprykerFeatures(currentFeatures, currentComposer));
-}
-
 function nextTarget(el, data) {
     return render(el, stepsToHitTarget(data));
 }
+
+nextTarget('next-target', { myComposerJSON, myComposerLOCK, releaseFeatures, releaseModules, architectureChanges, recommendedFeatures });
