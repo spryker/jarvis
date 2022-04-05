681a32466e48b27d55d108db42225082e99d77f0
Jeremy Fourna <jeremyfourna@hey.com>
2022-04-05 13:35:58 +0200

    WIP

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,59 @@
 <script>
-    import { projects } from "../stores/projects";
+    let projectName = '';
+    let files;
+    let composerJson;
+    let composerLock;
+    let errors = {
+        projectName: false,
+        composerFiles: false
+    };
+
+    $: if (files) {
+        // Note that `files` is of type `FileList`, not an Array:
+        // https://developer.mozilla.org/en-US/docs/Web/API/FileList
+        console.log(files);
+
+        for (const file of files) {
+            console.log(`${file.name}: ${file.size} bytes`);
+        }
+    }
+
+    function handleClick() {
+        if (projectName === '') {
+            errors.projectName = true;
+        }
+
+        if (files === undefined) {
+            errors.composerFiles = true;
+        }
+
+        console.log(errors);
+    }
+
+
 </script>
 
 <h2>New project analysis</h2>
-<form method="post" action="/project-analyser/">
+<form>
     <div class="form-group">
-        <label for="composerJson">Select your <code>composer.json</code> file</label>
-        <input type="file" class="form-control-file" id="composerJson" required accept=".json">
+        <label for="projectName">Your project name</label>
+        <input bind:value={projectName} type="text" class="form-control" id="projectName" required>
     </div>
     <div class="form-group">
-        <label for="composerLock">Select your <code>composer.lock</code> file</label>
-        <input type="file" class="form-control-file" id="composerLock" required accept=".lock">
+        <label for="composerJson">Select your <code>composer.json</code> and <code>composer.lock</code> files</label>
+        <input
+            bind:files
+            type="file"
+            class="form-control-file"
+            id="composerJson"
+            required="true"
+            multiple="true" 
+            accept=".json, .lock"
+        >
     </div>
 </form>
+<button
+    type="button"
+    class="btn btn-primary"
+    on:click|once={handleClick}
+>GO</button>
--- src/stores/projects.js
+++
@@ -1,3 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const projects = writable({ "lastCallToReleaseApp": null, "previousProjects": [], "lastProjectUsed": null });
