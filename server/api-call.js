a843952a9eda3c40f14aebb9b0945858e7575908
Jeremy Fourna <jeremyfourna@hey.com>
2023-01-23 10:12:50 +0100

    Fix standard issues

--- server/api-call.js
+++ server/api-call.js
@@ -1,27 +1,20 @@
-const request = require('request');
+import { request } from 'request'
 
-function getReleaseAppData(data, errorCallback, callback) {
-    const options = {
-        url: 'https://api.release.spryker.com/migration-analysis.json',
-        method: 'POST',
-        body: data,
-        json: true
-    };
+export function getReleaseAppData (data, errorCallback, callback) {
+  const options = {
+    url: 'https://api.release.spryker.com/migration-analysis.json',
+    method: 'POST',
+    body: data,
+    json: true
+  }
 
-    return request(options, (error, response, body) => {
-        const errorCode = body.code;
+  return request(options, (error, response, body) => {
+    const errorCode = body.code
 
-        if (error || errorCode === 404 || errorCode === 500) {
-    
-            return errorCallback(error);
-
-        } else {
-
-            return callback(body.result);
-
-        }
-    });
+    if (error || errorCode === 404 || errorCode === 500) {
+      return errorCallback(error)
+    } else {
+      return callback(body.result)
+    }
+  })
 }
-
-
-exports.getReleaseAppData = getReleaseAppData;
--- server/reset.js
+++ server/reset.js
@@ -1,12 +1,12 @@
-const { log } = require('./utils.js');
-const { updateConfigFile } = require('./file-system.js');
+import { log } from './utils.js'
+import { updateConfigFile } from './file-system.js'
 
 const resetObject = {
-    lastCallToReleaseApp: null,
-    previousProjects: [],
-    lastProjectUsed: null,
-    currentVersion: null
-};
+  lastCallToReleaseApp: null,
+  previousProjects: [],
+  lastProjectUsed: null,
+  currentVersion: null
+}
 
-updateConfigFile(resetObject);
-log('I have cleaned your local configuration. To use my services again please run -> node jarvis.js <path to your spryker project folder>');
+updateConfigFile(resetObject)
+log('I have cleaned your local configuration. To use my services again please run -> node jarvis.js <path to your spryker project folder>')
