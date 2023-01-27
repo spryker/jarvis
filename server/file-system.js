15068173b62cd65e7ad600c276702f1b423ab1fe
Jeremy Fourna <jeremyfourna@hey.com>
2023-01-27 15:44:14 +0100

    Change CommonJS to ES Modules plus eslint update

--- server/file-system.js
+++ server/file-system.js
@@ -1,167 +1,143 @@
-const fs = require('fs');
-const moment = require('moment');
-const {
-    __,
-    assoc,
-    compose,
-    concat,
-    equals,
-    forEach,
-    identity,
-    ifElse,
-    isNil,
-    lensProp,
-    map,
-    over,
-    prop
-} = require('ramda');
+import { writeFileSync, readFileSync } from 'fs'
+import moment from 'moment'
+import { __, assoc, compose, concat, equals, forEach, identity, ifElse, isNil, lensProp, map, over, prop } from 'ramda'
 
-function updateConfigFile(data) {
-    const newData = assoc('currentVersion', getCurrentVersion(), data);
+export function updateConfigFile (data) {
+  const newData = assoc('currentVersion', getCurrentVersion(), data)
 
-    fs.writeFileSync('config.json', JSON.stringify(newData), 'utf8');
+  writeFileSync('config.json', JSON.stringify(newData), 'utf8')
 
-    return newData;
+  return newData
 }
 
-function updateOnlyModuleFile(bool) {
-    const newData = ifElse(
-        equals(false),
-        () => `const noFeatures = false;`,
-        () => `const noFeatures = true;`
-    )(bool);
+export function updateOnlyModuleFile (bool) {
+  const newData = ifElse(
+    equals(false),
+    () => 'const noFeatures = false;',
+    () => 'const noFeatures = true;'
+  )(bool)
 
-    fs.writeFileSync('dist/release-app-data/no-features.js', newData, 'utf8');
+  writeFileSync('dist/release-app-data/no-features.js', newData, 'utf8')
 
-    return bool;
+  return bool
 }
 
-function updateMissingFeaturesFile(bool) {
-    const newData = ifElse(
-        equals(false),
-        () => `const missingFeatures = false;`,
-        () => `const missingFeatures = true;`
-    )(bool);
+export function updateMissingFeaturesFile (bool) {
+  const newData = ifElse(
+    equals(false),
+    () => 'const missingFeatures = false;',
+    () => 'const missingFeatures = true;'
+  )(bool)
 
-    fs.writeFileSync('dist/release-app-data/missing-features.js', newData, 'utf8');
+  writeFileSync('dist/release-app-data/missing-features.js', newData, 'utf8')
 
-    return bool;
+  return bool
 }
 
-function getConfig() {
-    return JSON.parse(fs.readFileSync('config.json', 'utf8'));
+export function getConfig () {
+  return JSON.parse(readFileSync('config.json', 'utf8'))
 }
 
-function getCurrentVersion() {
-    return prop('version', JSON.parse(fs.readFileSync('package.json', 'utf8')));
+export function getCurrentVersion () {
+  return prop('version', JSON.parse(readFileSync('package.json', 'utf8')))
 }
 
-function updateLastApiCall(config) {
-    const date = moment.utc();
-    const newConfig = compose(
-        over(
-            lensProp('previousProjects'),
-            map(ifElse(
-                p => p.projectName === config.lastProjectUsed,
-                assoc('lastCallToReleaseApp', date),
-                identity
-            ))),
-        assoc('lastCallToReleaseApp', date)
-    )(config);
+export function updateLastApiCall (config) {
+  const date = moment.utc()
+  const newConfig = compose(
+    over(
+      lensProp('previousProjects'),
+      map(ifElse(
+        p => p.projectName === config.lastProjectUsed,
+        assoc('lastCallToReleaseApp', date),
+        identity
+      ))),
+    assoc('lastCallToReleaseApp', date)
+  )(config)
 
-    return updateConfigFile(newConfig);
+  return updateConfigFile(newConfig)
 }
 
 // This function does some IO
 // getComposerData :: string -> [object]
-function getComposerData(path) {
-    return [{
-        path: './dist/my-composer-files/composerJSON.js',
-        data: fs.readFileSync(concat(path, '/composer.json'), 'utf8'),
-        stringStart: 'const myComposerJSON = ',
-        stringEnd: ';'
-    }, {
-        path: './dist/my-composer-files/composerLOCK.js',
-        data: fs.readFileSync(concat(path, '/composer.lock'), 'utf8'),
-        stringStart: 'const myComposerLOCK = ',
-        stringEnd: ';'
-    }];
+function getComposerData (path) {
+  return [{
+    path: './dist/my-composer-files/composerJSON.js',
+    data: readFileSync(concat(path, '/composer.json'), 'utf8'),
+    stringStart: 'const myComposerJSON = ',
+    stringEnd: ';'
+  }, {
+    path: './dist/my-composer-files/composerLOCK.js',
+    data: readFileSync(concat(path, '/composer.lock'), 'utf8'),
+    stringStart: 'const myComposerLOCK = ',
+    stringEnd: ';'
+  }]
 }
 
-function writeReleaseAppData(currentProject, data = undefined) {
-    const newData = isNil(data) ? JSON.parse(fs.readFileSync(`./tmp/${currentProject}.json`, 'utf8')) : data;
-    const p = prop(__, newData);
-    const files = [{
-        path: './dist/release-app-data/release-feature.js',
-        data: JSON.stringify(p('features')),
-        stringStart: 'const releaseFeatures = ',
-        stringEnd: ';'
-    }, {
-        path: './dist/release-app-data/release-module.js',
-        data: JSON.stringify(p('modules')),
-        stringStart: 'const releaseModules = ',
-        stringEnd: ';'
-    }, {
-        path: './dist/release-app-data/architecture-changes.js',
-        data: JSON.stringify(p('architectureChanges')),
-        stringStart: 'const architectureChanges = ',
-        stringEnd: ';'
-    }, {
-        path: './dist/release-app-data/recommended-features.js',
-        data: JSON.stringify(p('recommendedFeatures')),
-        stringStart: 'const recommendedFeatures = ',
-        stringEnd: ';'
-    }, {
-        path: './dist/release-app-data/detected-features.js',
-        data: JSON.stringify(p('detectedFeatures')),
-        stringStart: 'const detectedFeatures = ',
-        stringEnd: ';'
-    }, {
-        path: './dist/release-app-data/onboarding.js',
-        data: JSON.stringify(p('onboarding')),
-        stringStart: 'const onboarding = ',
-        stringEnd: ';'
-    }, {
-        path: './dist/release-app-data/current-version.js',
-        data: JSON.stringify(p('currentVersion')),
-        stringStart: 'const currentVersion = ',
-        stringEnd: ';'
-    }, {
-        path: './dist/release-app-data/product-releases.js',
-        data: JSON.stringify(p('productReleases')),
-        stringStart: 'const productReleases = ',
-        stringEnd: ';'
-    }, {
-        path: `./tmp/${currentProject}.json`,
-        data: JSON.stringify(newData),
-        stringStart: '',
-        stringEnd: ''
-    }];
+export function writeReleaseAppData (currentProject, data = undefined) {
+  const newData = isNil(data) ? JSON.parse(readFileSync(`./tmp/${currentProject}.json`, 'utf8')) : data
+  const p = prop(__, newData)
+  const files = [{
+    path: './dist/release-app-data/release-feature.js',
+    data: JSON.stringify(p('features')),
+    stringStart: 'const releaseFeatures = ',
+    stringEnd: ';'
+  }, {
+    path: './dist/release-app-data/release-module.js',
+    data: JSON.stringify(p('modules')),
+    stringStart: 'const releaseModules = ',
+    stringEnd: ';'
+  }, {
+    path: './dist/release-app-data/architecture-changes.js',
+    data: JSON.stringify(p('architectureChanges')),
+    stringStart: 'const architectureChanges = ',
+    stringEnd: ';'
+  }, {
+    path: './dist/release-app-data/recommended-features.js',
+    data: JSON.stringify(p('recommendedFeatures')),
+    stringStart: 'const recommendedFeatures = ',
+    stringEnd: ';'
+  }, {
+    path: './dist/release-app-data/detected-features.js',
+    data: JSON.stringify(p('detectedFeatures')),
+    stringStart: 'const detectedFeatures = ',
+    stringEnd: ';'
+  }, {
+    path: './dist/release-app-data/onboarding.js',
+    data: JSON.stringify(p('onboarding')),
+    stringStart: 'const onboarding = ',
+    stringEnd: ';'
+  }, {
+    path: './dist/release-app-data/current-version.js',
+    data: JSON.stringify(p('currentVersion')),
+    stringStart: 'const currentVersion = ',
+    stringEnd: ';'
+  }, {
+    path: './dist/release-app-data/product-releases.js',
+    data: JSON.stringify(p('productReleases')),
+    stringStart: 'const productReleases = ',
+    stringEnd: ';'
+  }, {
+    path: `./tmp/${currentProject}.json`,
+    data: JSON.stringify(newData),
+    stringStart: '',
+    stringEnd: ''
+  }]
 
-    return writeFiles(files);
-
+  return writeFiles(files)
 }
 
 // This function does some IO
 // writeFiles :: [object] -> [object]
-function writeFiles(listOfFiles) {
-    forEach(cur => fs.writeFileSync(cur.path, `${cur.stringStart}${cur.data}${cur.stringEnd}`, 'utf8'), listOfFiles);
+function writeFiles (listOfFiles) {
+  forEach(cur => writeFileSync(cur.path, `${cur.stringStart}${cur.data}${cur.stringEnd}`, 'utf8'), listOfFiles)
 
-    return listOfFiles;
+  return listOfFiles
 }
 
-function getComposerFilesFromPath(path) {
-    return compose(
-        writeFiles,
-        getComposerData
-    )(path);
+export function getComposerFilesFromPath (path) {
+  return compose(
+    writeFiles,
+    getComposerData
+  )(path)
 }
-
-exports.getConfig = getConfig;
-exports.getCurrentVersion = getCurrentVersion;
-exports.getComposerFilesFromPath = getComposerFilesFromPath;
-exports.updateConfigFile = updateConfigFile;
-exports.updateLastApiCall = updateLastApiCall;
-exports.updateMissingFeaturesFile = updateMissingFeaturesFile;
-exports.updateOnlyModuleFile = updateOnlyModuleFile;
-exports.writeReleaseAppData = writeReleaseAppData;
--- server/utils.js
+++ server/utils.js
@@ -1,44 +1,34 @@
-const {
-    complement,
-    drop,
-    isNil,
-    nth,
-    split
-} = require('ramda');
+import { complement, drop, isNil, nth, split } from 'ramda'
 
 // This function does some IO
 // log :: a -> a
-function log(content) {
-    console.log(...arguments);
-    return content;
+export function log (content) {
+  console.log(...arguments)
+  return content
 }
 
 // This function does some IO
 // cleanNodeInput :: [string] -> [string]
-function cleanNodeInput(args) {
-    return drop(2, args);
+export function cleanNodeInput (args) {
+  return drop(2, args)
 }
 
-function semVer(index, version) {
-    return Number(nth(index, split('.', version)));
+function semVer (index, version) {
+  return Number(nth(index, split('.', version)))
 }
 
-const semVerMajor = version => semVer(0, version);
+export const isNotNil = complement(isNil)
 
-const semVerMinor = version => semVer(1, version);
+const semVerMajor = version => semVer(0, version)
 
-const semVerPatched = version => semVer(2, version);
+const semVerMinor = version => semVer(1, version)
 
-function versionToNumber(version) {
-    const major = semVerMajor(version) * 1000000;
-    const minor = semVerMinor(version) * 1000;
-    const patch = semVerPatched(version);
+const semVerPatched = version => semVer(2, version)
 
-    return major + minor + patch;
+export function versionToNumber (version) {
+  const major = semVerMajor(version) * 1000000
+  const minor = semVerMinor(version) * 1000
+  const patch = semVerPatched(version)
+
+  return major + minor + patch
 }
-
-
-exports.cleanNodeInput = cleanNodeInput;
-exports.log = log;
-exports.isNotNil = complement(isNil);
-exports.versionToNumber = versionToNumber;
